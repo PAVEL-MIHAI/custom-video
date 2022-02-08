0c0c743012285f3300f243b05062596a041a973b
PAVEL-MIHAI <96009957+PAVEL-MIHAI@users.noreply.github.com>
2022-02-08 20:10:30 +0200

    fix: button

--- index.js
+++ index.js
@@ -1,33 +1,44 @@
 
-console.log("пока только сверстал 5 баллов ");
-console.log("-");
+console.log(" 5 баллов верстка, нет бара ");
+console.log(" 10 баллов кнопке в центре работает на play/pause");
+console.log(" Пока это всё, что успел");
 
 // -------смена языка---------------------------
 // const portfolioBtn = document.querySelector('.play-btn');
 
-var videoEl = document.getElementsByTagName('.video')[0],
-    playBtn = document.getElementById('.play-btn');
+// var videoEl = document.getElementsByTagName('.video')[0],
+//     playBtn = document.getElementById('.play-btn');
 
 
-playBtn.addEventListener('click', function () {
-  if (videoEl.paused) {
-      videoEl.play();
-  } else {
-      videoEl.pause();
-  }
+// playBtn.addEventListener('click', function () {
+//   if (videoEl.paused) {
+//       videoEl.play();
+//   } else {
+//       videoEl.pause();
+//   }
 
-document.getElementById(".video").style.zIndex = "1";
+// document.getElementById(".video").style.zIndex = "1";
   
-}, false);
+// }, false);
 
+const playBtn = document.querySelector('.play-btn');
+const video = document.querySelector('video')
 
-
-portfolioBtn.addEventListener('click', () => {
+playBtn.addEventListener('click', () => {
   
   
-  // ------ цвет др кнопок---------
-  portfolioBtnRus.style.color = '#bdae82';
-  // ----цвет кнопки-------
-  portfolioBtnEn.style.color = '#fff';
+  document.querySelector('video').play();
+  playBtn.style.zIndex = '-1'; 
+     
+   
+});
+
+video.addEventListener('click', () => {
+  
+  
+  document.querySelector('video').pause();
+  playBtn.style.zIndex = '0'; 
+     
+   
 });
 
